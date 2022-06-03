6876ea872d295b474cc1c97da0b0163c69f4aa84
EyssyDev <193286@ids.upchiapas.edu.mx>
2022-06-03 01:40:54 -0500

    Ahora devuelve perros

--- server.js
+++ server.js
@@ -1,39 +1,94 @@
 const express = require('express')
-const mysql = require('mysql')
-const myconn = require('express-myconnection')
-
-const routes = require('./routes')
 
 const app = express()
 app.set('port', process.env.PORT || 9000)
-const dbOptions = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'api-simple'
-}
 
-app.use(myconn(mysql, dbOptions, 'single'))
 app.use(express.json())
 
 app.get('/', (req, res)=>{
-    res.send('La API esta funciona correctamente.')
+    res.send('La API esta funciona correctamente, ingrese la raza de un perro para saber mas de esta.')
 })
 
-app.get('/1', (req, res)=>{
-    res.send('Perro')
+app.get('/Bulldog', (req, res)=>{
+    res.json(
+        {
+            "Tamaño": "Pequeño",
+            "Tiempo de Vida": "8 años",
+            "Velocidad": "27Km/h"
+        }
+    )
 })
 
-app.get('/2', (req, res)=>{
-    res.send('Gato')
+app.get('/Labrador', (req, res)=>{
+    res.json(
+        {
+            "Tamaño": "Grande",
+            "Tiempo de Vida": "7 años",
+            "Velocidad": "44Km/h"
+        }
+    )
 })
 
-app.get('/3', (req, res)=>{
-    res.send('Cotorro')
+app.get('/Pastor_Aleman', (req, res)=>{
+    res.json(
+        {
+            "Tamaño": "Grande",
+            "Tiempo de Vida": "7 años",
+            "Velocidad": "42Km/h"
+        }
+    )
 })
 
-app.use('/api', routes)
+app.get('/Poodle', (req, res)=>{
+    res.json(
+        {
+            "Tamaño": "Mediano",
+            "Tiempo de Vida": "10 años",
+            "Velocidad": "32Km/h"
+        }
+    )
+})
+
+app.get('/Chihuahua', (req, res)=>{
+    res.json(
+        {
+            "Tamaño": "Pequeño",
+            "Tiempo de Vida": "13 años",
+            "Velocidad": "20Km/h"
+        }
+    )
+})
+
+app.get('/Pug', (req, res)=>{
+    res.json(
+        {
+            "Tamaño": "Pequeño",
+            "Tiempo de Vida": "10 años",
+            "Velocidad": "23Km/h"
+        }
+    )
+})
+
+app.get('/Shiba', (req, res)=>{
+    res.json(
+        {
+            "Tamaño": "Mediano",
+            "Tiempo de Vida": "7 años",
+            "Velocidad": "30Km/h"
+        }
+    )
+})
+
+app.get('/Galgo', (req, res)=>{
+    res.json(
+        {
+            "Tamaño": "Mediano",
+            "Tiempo de Vida": "9 años",
+            "Velocidad": "72Km/h"
+        }
+    )
+})
+
 
 app.listen(app.get('port'), ()=>{
     console.log('server running on port', app.get('port'))
